5f1f3affc30bfe99596949dac715ac3150c5582f
DiegoTeacher <diego.baratto@ifp.es>
2024-04-05 14:21:00 +0200

    OOP (creación de clases)

---
+++ Assets/Scripts/POO/PooTest.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PooTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        RPMI.Vector3 vector3_2 = new RPMI.Vector3("Vector Yanes", 0, 10, 9);
+        print(vector3_2.GetName() + " " + vector3_2.x);
+        print("El vector " + vector3_2.GetName() + " mide " + vector3_2.Modulo()); // 13.45
+        vector3_2.Add(5, 10, -45);
+        vector3_2.Add(new RPMI.Vector3("a", 1, 1, 1)); // 6, 21, -35
+        vector3_2.Mul(10); // 60, 210, -350
+        print("El vector " + vector3_2.GetName() + " mide " + vector3_2.Modulo()); // 412.553
+    }
+}
